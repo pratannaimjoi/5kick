a6dbbdeb234911dc825e414192e62c9c26442c49
Pratan naimjoi <52064201+pratannaimjoi@users.noreply.github.com>
2020-02-26 09:36:12 +0700

    Update GACSender.py

--- GACSender.py
+++ GACSender.py
@@ -2,7 +2,7 @@ import requests,json
 
 class send():
     def __init__(self, pnum):
-        resp = requests.post('https://p.grabtaxi.com/api/passenger/v2/profiles/register', data={'phoneNumber': pnum,'countryCode': 'TH','name': 'Noxtian','email': 'noxtian@noxt.cf','deviceToken': '*'}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'})
+        resp = requests.post('https://p.grabtaxi.com/api/passenger/v2/profiles/register', data={'phoneNumber': pnum,'countryCode': 'TH','name': 'Noxtian','email': 'noxtian@noxt.cf','deviceToken': '*'}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'})
         print(resp.status_code)
         if resp.status_code == 200:
             self.responseNum = 0
